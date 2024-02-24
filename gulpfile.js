de869c2b0f8a71e974c3854e62bcf9400d1cc126
MrRezai <mrrz.portal@gmail.com>
2024-02-24 12:03:51 +0330

    1

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,7 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-      =========================================================
-      * CAW World - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      How are you today ?
       
       */`)
     )
@@ -30,25 +18,7 @@ gulp.task("licenses", async function () {
   gulp
     .src("build/index.html", { base: "./" })
     .pipe(
-      gap.prependText(`<!--
-      /*!
-      
-      =========================================================
-      * CAW World - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-      
-      */
-      -->`)
+      gap.prependText(`<!-- How are you today ? -->`)
     )
     .pipe(gulp.dest("./", { overwrite: true }));
 
@@ -58,19 +28,7 @@ gulp.task("licenses", async function () {
     .pipe(
       gap.prependText(`/*!
 
-      =========================================================
-      * CAW World - v1.0.1
-      =========================================================
-      
-      * Product Page: https://www.creative-tim.com/product/purity-ui-dashboard
-      * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-      * Licensed under MIT (https://github.com/creativetimofficial/purity-ui-dashboard/blob/master/LICENSE.md)
-      
-      * Design by Creative Tim & Coded by Simmmple
-      
-      =========================================================
-      
-      * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      How are you today ?
       
       */`)
     )
